a00b5d136ac28911d815ad8c2cd7e10b40241c51
zhengxiaowai <h1x2y3awalm@gmail.com>
2016-03-26 00:13:21 +0800

    完成

---
+++ index.js
@@ -0,0 +1,52 @@
+var qiniu = require("qiniu");
+var shortid = require('shortid');
+var _ = require('underscore');
+var path = require('path');
+var fs = require('fs');
+
+
+var configPath = path.join(process.env.HOME, '.transrc');
+var config = readConfig(configPath);
+
+
+qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
+qiniu.conf.SECRET_KEY = config.SECRET_KEY;
+domain = config.domain;
+bucket = config.bucket;
+
+
+function uptoken(bucket, key) {
+  var putPolicy = new qiniu.rs.PutPolicy(bucket+":"+key);
+  return putPolicy.token();
+}
+
+
+function uploadFile(uptoken, key, localFile) {
+  var extra = new qiniu.io.PutExtra();
+    qiniu.io.putFile(uptoken, key, localFile, extra, function(err, ret) {
+      if(!err) {
+        process.stdout.write(domain + ret.key + '\n');
+      } else {
+        process.stderr.write(err);
+      }
+  });
+}
+
+
+function readConfig (configPath) {
+    return JSON.parse(fs.readFileSync(configPath, 'utf8'));
+}
+
+
+var args = process.argv.splice(2);
+_.each(args, function(item, index) {
+    var extname = path.extname(item);
+    var randomId = shortid.generate();
+    var newFileName = randomId + extname;
+
+    token = uptoken(bucket, newFileName);
+    uploadFile(token, newFileName, item);
+});
+
+
+
